8d4d299d15516a68ec9007ae8616c3a42a0152eb
cedev935 <cedev935@gmail.com>
2023-08-05 11:46:40 +0530

    Navigation Enhanced

--- src/features/nav/Desktop.jsx
+++ src/features/nav/Desktop.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { styled } from 'styled-components';
+import glowingAnimation from '../Animations/GlowingText';
 
 const Desktop = () => (
   <Wrap>
-    <a href="#about"><li>About</li></a>
     <a href="#projects"><li>Projects</li></a>
+    <a href="#projects"><li>Skills</li></a>
+    <a href="#about"><li>About</li></a>
     <a href="#contact"><li>Contact</li></a>
   </Wrap>
 );
@@ -14,6 +16,25 @@ const Wrap = styled.ul`
   width: 60%;
   justify-content: space-around;
   list-style-type:  none;
+  font-size: 1.2rem;
+  a {
+    color: #c3b0ec;
+    border-radius: .3rem;
+    text-decoration: none;
+    position: relative;
+    animation: ${glowingAnimation} 2s infinite;
+    li {
+      width: 6rem;
+      text-align: center;
+      padding: .1rem .3rem;
+    }
+
+    &:hover {
+      background-color: #c3b0ec;
+      color: black;
+      transition: all .5s;
+    }
+  }
 `;
 
 export default Desktop;
