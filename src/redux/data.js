322858ba7187c4054d8c1ec58efaf332ac7a61cb
cedev935 <cedev935@gmail.com>
2023-08-08 10:49:18 +0530

    Project -Images Batch - 1

---
+++ src/redux/data.js
@@ -0,0 +1,208 @@
+const projectCard = [
+  {
+    title: 'Market Analysis',
+    projectMaker: [
+      'Adarsh',
+      'Full-Stack Dev',
+      '2023',
+    ],
+    lilDescription: `
+      This is a React-Based Project, in this website you can analyze your stocks 
+      and compare them to optimize your profit, it works with the financial API 
+      `,
+    description: `
+      This is a React-Based Project, in this website you can analyze your 
+      stocks and compare them to optimize your profit, it works with the 
+      financial API, and apart from this you will find responsive design 
+      clean UI, and in the future maybe we have some more features for 
+      checking bitcoin and searching company stock by name and a detailed 
+      navigation bar 
+      `,
+    techList: [
+      'React',
+      'Context',
+      'Styled',
+      'CSS',
+    ],
+    link: [
+      'https://stock-analysis-io.netlify.app/',
+      'https://github.com/PowerLevel9000/stock-market/',
+    ],
+    image: 'projects-img/stock-market/desktop.png',
+    imagePop: 'projects-img/stock-market/all-devices-black.png',
+  },
+  {
+    title: 'Shopholic',
+    projectMaker: [
+      'Adarsh',
+      'Full-Stack Dev',
+      '2023',
+    ],
+    lilDescription: `
+      This project is React oriented, I build this while learning react and redux however i used context 
+      API in this project, It is a E-Commerce website for mobile selling  
+      `,
+    description: `
+      This project is React oriented, I build this while learning react and redux however i used context 
+      API in this project, It is a E-Commerce website for mobile selling, in this you will see Cart funtionality,
+      , Payment Getway,  add to cart, remove from cart, Quantity increment decrement for products,
+      <small>work in progress ....</small>
+      `,
+    techList: [
+      'React',
+      'Context',
+      'Styled',
+      'CSS',
+    ],
+    link: [
+      'https://react-e-commerce-store-react.netlify.app/',
+      'https://github.com/PowerLevel9000/e-commerce/',
+    ],
+    image: 'projects-img/shopholic/desktop.png',
+    imagePop: 'projects-img/shopholic/all-devices-black.png',
+
+  },
+  {
+    title: 'Leader Board',
+    projectMaker: [
+      'Adarsh',
+      'Full-Stack Dev',
+      '2022',
+    ],
+    lilDescription: `
+      This project is build with an API for any game organization to add their Scores
+      on the server by the player I know there is an issue anyone could add scores lol.
+      <small>it's mobile version is not ready yet!</small>
+      `,
+    description: `
+      This project is build with an API for any game organization to add their Scores
+      on the server by the player I know there is an issue anyone could add scores lol,
+      but as this project is build for learning API not building the actual prof. leaderboard.<small>
+      it's mobile version is not ready yet!</small>
+      `,
+    techList: [
+      'SASS',
+      'JavaScript',
+      'API',
+      'HTML',
+    ],
+    link: [
+      'https://powerlevel9000.github.io/LeaderboardWithApi/dist/',
+      'https://github.com/PowerLevel9000/LeaderboardWithApi/',
+    ],
+    image: 'projects-img/leaderboard/desktop.png',
+    imagePop: 'projects-img/leaderboard/all-devices-black.png',
+
+  },
+  {
+    title: 'Event Template',
+    projectMaker: [
+      'Adarsh',
+      'Full-Stack Dev',
+      '2022',
+    ],
+    lilDescription: `This template is for organizing any event in future. It is build dynamically It means you just need to
+      upload data in it and done `,
+    description: `
+        This project build with pure HTML CSS and JavaScript. IT is mostly focus on frontend designing also 
+        it is build dynamically so if you want to organize any event you just need to edit data and you are 
+        good to go. i learn many things while doing this project like core flexbox grid and complex navigation bars
+      `,
+    techList: [
+      'HTML',
+      'CSS',
+      'javaScript',
+      'GitHub',
+    ],
+    link: [
+      'https://powerlevel9000.github.io/Dynamic-Event-Template/',
+      'https://github.com/PowerLevel9000/Dynamic-Event-Template/',
+    ],
+    image: 'projects-img/event/desktop.png',
+    imagePop: 'projects-img/event/all-devices-black.png',
+
+  },
+  {
+    title: 'Wikipedia search',
+    projectMaker: [
+      'Adarsh',
+      'Full-Stack Dev',
+      '2022',
+    ],
+    lilDescription: `This project is build with simple implementation of javascript and api. I build this in my learning
+      period with the help of <a href="https://www.javascripttutorial.net/">Javascript Tutorial</a>.`,
+    description: `
+        This project is build with simple implementation of javascript and api. I build this in my learning
+        period with the help of <a href="https://www.javascripttutorial.net/">Javascript Tutorial</a>.
+        to search anything you just need to type there and you will find relevant Wikipedia links to read stuffs
+        and if you like my project give it a star on github. click on source button
+      `,
+    techList: [
+      'HTML',
+      'CSS',
+      'javaScript',
+    ],
+    link: [
+      'https://powerlevel9000.github.io/JavaScript-smallProjects/Wikipedia-Search-App/',
+      'https://github.com/PowerLevel9000/JavaScript-smallProjects/tree/master/Wikipedia-App/',
+    ],
+    image: 'projects-img/wikipedia-search/desktop.png',
+    imagePop: 'projects-img/wikipedia-search/all-devices-black.png',
+
+  },
+  {
+    title: 'Awesome Book',
+    projectMaker: [
+      'Adarsh',
+      'Full-Stack Dev',
+      '2023',
+    ],
+    lilDescription: `This project is build for organizing your favorite books and their authors. It is build with implementation of 
+      localstorage`,
+    description: `
+        This project is build for organizing your favorite books and their authors. It is build with implementation of 
+        localStorage, and dynamic pages i.e in single page all there pages is nested. some of projects is had been reviewed and
+        approved by microverse professional coders 
+      `,
+    techList: [
+      'CSS',
+      'JavaScript',
+      'localStorage',
+      'HTML',
+    ],
+    link: [
+      'https://powerlevel9000.github.io/Awesome-Books/dist/',
+      'https://github.com/PowerLevel9000/Awesome-Books/',
+    ],
+    image: 'projects-img/awesome-books/desktop.png',
+    imagePop: 'projects-img/awesome-books/all-devices-black.png',
+
+  },
+  {
+    title: 'Weather App',
+    projectMaker: [
+      'Adarsh',
+      'Full-Stack Dev',
+      '2022',
+    ],
+    lilDescription: `This project is build with simple implementation of javascript and api. I build this in my learning
+      period with the help of <a href="https://www.javascripttutorial.net/">Javascript Tutorial</a>.`,
+    description: `
+        This project is build with simple implementation of javascript and api. I build this in my learning
+        period with the help of my friend Coding Freak.
+        Here you can see weather status of each and every city around the world and background changes according to city weather
+        and if you like my project give it a star on github. click on source button
+      `,
+    techList: [
+      'HTML',
+      'CSS',
+      'javaScript',
+    ],
+    link: [
+      'https://powerlevel9000.github.io/JavaScript-smallProjects/Weather-app/',
+      'https://github.com/PowerLevel9000/JavaScript-smallProjects/Weather-app/',
+    ],
+    image: 'projects-img/weather-app/desktop.png',
+    imagePop: 'projects-img/weather-app/all-devices-black.png',
+  },
+];
